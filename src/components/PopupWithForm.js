fce27c32cd2ff37b5124eec5e1a31fd6d2011850
muratovnur <juzetty01@gmail.com>
2022-10-21 21:02:18 +0600

    iter2 fix: changed input submit to button

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,11 +1,11 @@
 function PopupWithForm(props) {
   return (
-    <div className={`${props.isOpen && "popup_opened"} popup popup_type_${props.name}`} /*edit-profile*/> 
+    <div className={`${props.isOpen && "popup_opened"} popup popup_type_${props.name}`}> 
       <div className="popup__container">
         <form name={`form-${props.name}`} className="form" noValidate="">
           <h2 className="form__title">{props.title}</h2>
           {props.children}
-          <input type="submit" className="form__submit" value="Сохранить"/>
+          <button type="button" className="form__submit">Сохранить</button>
         </form>
         <button type="button" className="popup__close-btn" onClick={props.onClose}/>
       </div>
